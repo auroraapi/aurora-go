1150f4d657bb850858ff20a977c0833d2ff4046f
Nikhil Kansal <nkansal96@gmail.com>
2018-02-01 20:05:35 -0800

    add basic config

--- aurora.go
+++ aurora.go
@@ -1,1 +1,13 @@
 package aurora
+
+import (
+	"github.com/nkansal96/aurora-go/config"
+)
+
+// Config is an alias for config.C so that the user can easily
+// override the SDK configuation by typing something like:
+//
+//   aurora.Config.ApplicationID    = "My ID"
+//   aurora.Config.ApplicationToken = "My Token"
+//
+var Config = config.C
--- config/config.go
+++ config/config.go
@@ -1,1 +1,17 @@
 package config
+
+// Config configures the parameters that this SDK will use to operate
+type Config struct {
+	// ApplicationID is the value to send as the `X-Application-ID` header
+	ApplicationID string
+
+	// ApplicationToken is the value to send as the `X-Application-Token` header
+	ApplicationToken string
+
+	// DeviceID is the value to send as the `X-Device-ID` header (optional)
+	DeviceID string
+}
+
+// C is an instance of the above config (with default values). It's exported
+// so that all packages can use it
+var C = &Config{}
