fe20154418c25530d5d349a9a079e60026971a7f
Rainfur <meltonaerik@gmail.com>
2018-02-04 15:29:22 -0800

    Work on TTS

--- api/tts.go
+++ api/tts.go
@@ -2,12 +2,29 @@ package api
 
 import (
 	"net/http"
+
 	"github.com/nkansal96/aurora-go/audio"
 )
 
 func GetTTS(string text) (*audio.File, error) {
-	return nil, nil
+	// Create GET request
+	req, err := http.NewRequest("GET", baseURL+ttsEndpoint, &text)
+	if err != nil {
+		return nil, err
+	}
+
+	client := getClient()
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	err = handleError(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	// Take data from server, put into audio.File
+	var tts audio.File
 }
-
-
-
