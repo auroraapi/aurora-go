ea5d03bec9709f6a499a11b14a51123038d303a1
Nathan Yang <nathanyang@engineering.ucla.edu>
2018-02-04 17:06:43 -0800

    Implemented Interpret API

--- api/interpret.go
+++ api/interpret.go
@@ -1,5 +1,12 @@
 package api
 
+import (
+	"net/http"
+	"net/url"
+	"fmt"
+	"encoding/json"
+)
+
 // InterpretResponse is the response returned by the API if the text was
 // successfully able to be interpreted
 type InterpretResponse struct {
@@ -19,5 +26,33 @@ type InterpretResponse struct {
 // GetInterpret queries the API with the provided text and returns
 // the interpreted response
 func GetInterpret(text string) (*InterpretResponse, error) {
-	return nil, nil
+	interpretUrl := baseURL + interpretEndpoint
+	queryStr := url.QueryEscape(text)
+	queryUrl := fmt.Sprintf("%s?text=%s", interpretUrl, queryStr)
+
+	client := getClient()	// := ?
+	req, err := http.NewRequest("GET", queryUrl, nil)
+
+	if err != nil {
+		return nil, err // come back to this 
+	}
+
+	req.Header = *getHeaders()
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err // come back to this 
+	}
+
+	err = handleError(res)
+	if err != nil {
+		return nil, err
+	}
+
+	resStruct := new(InterpretResponse)
+	if err := json.NewDecoder(res.Body).Decode(resStruct); err != nil {
+		return nil, err
+	}
+
+	return resStruct, nil
 }
