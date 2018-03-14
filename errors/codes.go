e5ed12a9e6cbea340662b853e6000f9df0d1f719
Nikhil Kansal <nkansal96@gmail.com>
2018-03-13 20:41:25 -0700

    Update codes.go

--- errors/codes.go
+++ errors/codes.go
@@ -14,9 +14,9 @@ const (
 // errorMessages converts an error code to its corresponding message
 var errorMessages = map[ErrorCode]string{
 	SpeechNilAudio: "The audio file was nil. In order to convert a Speech object to Text, it must have a valid audio file. Usually, this means you created a Speech object that wasn't created using one of the Listen methods.",
-	WAVCorruptFile: "The wav file was corrupted. The wav file sent in does not have a correctly formatted RIFF header. Check the file to make sure it was not corrupted or incomplete.",
-	AudioFileOutputStreamNotOpened: "The audio stream was unable to be opened. Portaudio encountered an error in opening the file stream which is usually do to an error in connecting to the input and/or output device. ",
-	AudioFileNotWritableStream: "The data could not be written into the stream. This may be due to attempting to write to a callback stream, trying to write to an input only stream, the buffer had incorrect parameters, or the stream is not open.",
+	WAVCorruptFile: "The WAV file was corrupted and did not have a correctly formatted RIFF header. Check the file to make sure it was not corrupted or incomplete.",
+	AudioFileOutputStreamNotOpened: "PortAudio encountered an error in opening the audio stream, which is usually due to an error in connecting to the input and/or output device.",
+	AudioFileNotWritableStream: "The data could not be written into the stream. You may have attempted to write to a callback stream, tried to write to an input-only stream, created a buffer with incorrect parameters, or did not open the stream at all.",
 }
 
 
